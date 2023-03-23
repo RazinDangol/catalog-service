4fde9acfa0df0799c22f1003ff1ad3b1b631581d
razindangol <dangolrajin@gmail.com>
2023-03-22 23:12:44 -0400

    Added Auditing and publisher

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -3,7 +3,7 @@ package com.polarbookshop.catalogservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
